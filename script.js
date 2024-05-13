893edb8e0b209b9c62bfae9eafd5642e355911da
Olive97424 <defaudolivier@gmail.com>
2024-05-14 00:52:02 +0400

    modern

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ function createFilmElement(film) {
 
     const img = document.createElement('img');
     img.src = film.image;
-    img.alt = film.titre;
+    img.alt = film.titre.replace(/_/g, ' ');
+    img.onload = () => img.classList.add('loaded');
 
     const caption = document.createElement('div');
-    caption.textContent = film.titre;
+    caption.textContent = film.titre.replace(/_/g, ' ');
 
     anchor.appendChild(img);
     filmDiv.appendChild(anchor);
     filmDiv.appendChild(caption);
+
+    setTimeout(() => filmDiv.classList.add('show'), 100); // Add 'show' class for animation delay
 
     return filmDiv;
 }
@@ -53,11 +56,34 @@ function filterFilmsByGenre() {
     const selectedGenre = document.getElementById('genreSelect').value.toLowerCase();
     const gallery = document.getElementById('filmGallery');
     gallery.innerHTML = '';
-    films.forEach(film => {
-        if (film.genres.map(g => g.toLowerCase()).includes(selectedGenre) || selectedGenre === '') {
-            gallery.appendChild(createFilmElement(film));
+    films.forEach((film, index) => {
+        if (!film.genres.length && selectedGenre === '' || film.genres.map(g => g.toLowerCase()).includes(selectedGenre)) {
+            const filmElement = createFilmElement(film);
+            gallery.appendChild(filmElement);
+            setTimeout(() => {
+                filmElement.classList.add('show');
+            }, 100 * index);
         }
     });
 }
 
-window.onload = populateFilms;
+function populateGenres() {
+    const genreSet = new Set();  // Use a Set to avoid duplicates
+    films.forEach(film => {
+        film.genres.forEach(genre => {
+            genreSet.add(genre);
+        });
+    });
+    const genreSelect = document.getElementById('genreSelect');
+    genreSet.forEach(genre => {
+        const option = document.createElement('option');
+        option.value = genre.toLowerCase();
+        option.textContent = genre;
+        genreSelect.appendChild(option);
+    });
+}
+
+window.onload = () => {
+    populateFilms();
+    populateGenres();
+};
